30049af79d6dcb4f0443bc8d456a033e9412b034
Constant V <constantvallee@gmail.com>
2020-07-12 14:34:21 -0400

    Add CDS websites 🎪

---
+++ _data/sites/cds.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "CDS", // optional, falls back to object key
+	description: "Cirque du Soleil web sites",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		freshChrome: "site",
+	},
+	urls: [
+		"https://www.cirquedusoleil.com",
+		"https://www.cirquedusoleil.com/cirqueconnect",
+		"https://www.cirquedusoleil.com/drawn-to-life",
+		"https://www.cirquedusoleil.com/las-vegas",
+    "https://www.blueman.com/",
+    "https://www.pawpatrollive.com/",
+	]
+};
